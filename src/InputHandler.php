23a531b1f98cb41a98c8f397c7ea171e187d9bf8
Chris Cornutt <chris.cornutt@hp.com>
2015-01-28 15:07:56 -0600

    adding more untested methods

--- src/InputHandler.php
+++ src/InputHandler.php
@@ -56,4 +56,49 @@ class InputHandler
     {
         return 'quux';
     }
+
+    public function test14()
+    {
+        return 'baz';
+    }
+    public function test15()
+    {
+        return 'quux';
+    }
+    public function test16()
+    {
+        return 'quux';
+    }
+    public function test17()
+    {
+        return 'quux';
+    }
+    public function test18()
+    {
+        return 'quux';
+    }
+    public function test19()
+    {
+        return 'quux';
+    }
+    public function test20()
+    {
+        return 'quux';
+    }
+    public function test21()
+    {
+        return 'quux';
+    }
+    public function test22()
+    {
+        return 'quux';
+    }
+    public function test23()
+    {
+        return 'quux';
+    }
+    public function test24()
+    {
+        return 'quux';
+    }
 }
